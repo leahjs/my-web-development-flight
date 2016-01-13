833d5e2b39e40e9e06f3f151f448301fcda5b113
leahjs <leahj.suter@gmail.com>
2016-01-13 17:37:23 -0500

    attempt at fixing sidebar

--- wp-content/themes/flight-theme/page-right-sidebar.php
+++ wp-content/themes/flight-theme/page-right-sidebar.php
@@ -11,13 +11,6 @@
         <div class="row">
           <!-- Primary Column -->
           <div class="small-12 medium-4 medium-pull-8 columns">
-            <div class="secondary">
-              <h2 class="module-heading">Sidebar</h2>
-            </div>
-          </div>
-
-          <!-- Secondary Column -->
-          <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
             <div class="primary">
               <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
@@ -29,6 +22,13 @@
                 <p><?php _e( 'Sorry No Pages' ); ?></p>
 
               <?php endif ?>
+            </div>
+          </div>
+
+          <!-- Secondary Column -->
+          <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
+            <div class="secondary">
+              <h2 class="module-heading">Sidebar</h2>
             </div>
           </div>
         </div>
