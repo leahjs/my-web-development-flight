7314cb9c1193daeb2c26d5500ba9f13843ff6e63
leahjs <leahj.suter@gmail.com>
2016-01-13 16:14:18 -0500

    testing the index.html

--- wp-content/themes/flight-theme/index.php
+++ wp-content/themes/flight-theme/index.php
@@ -1,10 +1,13 @@
 <?php get_header(); ?>
+<h1>1This is index.php and I better see a picture above this text!!!</h1>
+
 
 <section class="row">
   <img src="<?php bloginfo('template_url'); ?>/img/rocket_tours_blogtop.jpg" alt="Hero-image" />
 <h1>This is index.php and I better see a picture above this text!!!</h1>
 </section>
 
+<h1>2This is index.php and I better see a picture above this text!!!</h1>
 
 
 <?php get_footer(); ?>
