a6b9ab47b18ba9b20f78f88af48157dae2928485
leahjs <leahj.suter@gmail.com>
2016-01-13 12:32:27 -0500

    put def. themes in a dir and copied wp dir and made my own theme

--- wp-content/themes/flight-theme/index.php
+++ wp-content/themes/flight-theme/index.php
@@ -3,12 +3,16 @@
 <section class="row">
 
    <?php if ( have_posts() ): ?>
-   <h2>Latest Posts</h2>   
+   <h2>Latest Posts</h2>
    <?php while ( have_posts() ) : the_post(); ?>
       <article>
          <h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
          <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
             <?php the_content(); ?>
+
+            <p>
+              Hello Everyone!
+            </p>
       </article>
    <?php endwhile; ?>
    <?php else: ?>
