bdc41664edcc5fe30636b6c92976c87329c9324b
leahjs <leahj.suter@gmail.com>
2016-01-13 15:08:49 -0500

    set up header and footer.php

--- wp-content/themes/flight-theme/footer.php
+++ wp-content/themes/flight-theme/footer.php
@@ -1,13 +1,14 @@
       	</main>
 
       </div><!-- /.site -->
-
-      <footer class="site-footer">
+    <div class="footer-clear"></div>
+      <footer class="site-footer row no-max pad ">
         &copy;<?php echo date('Y'); ?>
         <a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
       </footer>
 
+
     <?php wp_footer(); ?>
-    
+
   </body>
 </html>
--- wp-content/themes/flight-theme/functions.php
+++ wp-content/themes/flight-theme/functions.php
@@ -15,5 +15,26 @@ require_once(get_stylesheet_directory() . '/includes/post-metas.php');
 require_once(get_stylesheet_directory() . '/includes/acf-hooks.php');
 
 
+function flight_theme_styles(){
+  wp_enqueue_style( 'foundtion_css', get_template_directory_uri() . '/css/foundation.css' );
+  wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+  wp_enqueue_style( 'google_css', 'https://fonts.googleapis.com/css?family=Montserrat:400,700' );
+  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+}
+
+add_action( 'wp_enqueue_scripts', 'flight_theme_styles' );
+
+
+function flight_theme_js() {
+  wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false);
+  wp_enqueue_script( 'foundation_js', get_template_directory_uri() . 'js/foundation.min.js', array('jquery'), '', true );
+  wp_enqueue_script( 'app_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );
+  wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/main.js', '', '', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'leahjs_theme_js' );
+
+
+
 
 ?>
